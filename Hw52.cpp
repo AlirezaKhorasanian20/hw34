6df8b78a8ec9989b41e012b96092bb609d35d0ba
Alireza khorasanian <117563866+AlirezaKhorasanian20@users.noreply.github.com>
2022-11-13 22:28:46 +0330

    Add files via upload

---
+++ Hw52.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char ch;
+	cout<<"Enter Character : ";
+	cin>>ch;
+	switch(ch)
+	{
+		case 'O':
+			cout<<"VOWEL";
+			break;
+		case 'I':
+			cout<<"VOWEL";
+			break;
+		case 'E':
+			cout<<"VOWEL";
+			break;
+		case 'A':
+			cout<<"VOWEL";
+			break;
+		case 'U':
+			cout<<"VOWEL";
+			break;
+	}
+	return 0;
+}
